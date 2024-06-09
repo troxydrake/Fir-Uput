0be525e1cf372b0f1d9a90cd6bb1ec15ac2418b6
troxydrake <158440744+troxydrake@users.noreply.github.com>
2024-06-09 12:27:43 +0700

    Update bot.py

--- Uputt/modules/bot/bot.py
+++ Uputt/modules/bot/bot.py
@@ -65,7 +65,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     this_command = f"──「 **Help For {str(modul_name).upper()}** 」──\n\n"
     for x in commands:
         this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-    this_command += "© @BeeXDomp"
+    this_command += "© @ZannXBumb"
     bttn = [
         [InlineKeyboardButton(text="Return", callback_data="reopen")],
     ]
